daffbc100c8ab6f8f762a398d24612f565ade089
Michail <hushokas@gmail.com>
2020-04-11 15:39:13 +0300

    [001]add: transition component implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,18 +8,44 @@ import List from "./components/List/List";
 
 const App = () => {
 
-  const[show, setShow] = useState(false);
+  const [show, setShow] = useState({
+    modalIsOpen: false,
+    showBlog: false,
+  });
 
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        {show && <Modal show={show} closed={()=>setShow(false)} />}
-        {show && <Backdrop show={show} />}
-        <button className="Button" onClick={()=>setShow(true)}>Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
+
+
+  return (
+    <div className="App">
+      <h1>React Animations</h1>
+      <button onClick={() => setShow(prevState => ({ ...prevState, showBlog: !prevState.showBlog }))}>toggle</button>
+      <Transition
+        in={show.showBlog} 
+        timeout={1000}
+        mountOnEnter
+        unmountOnExit
+        >
+        {show => (
+          <div style={{
+            backgroundColor: "red",
+            width: 100,
+            height: 100,
+            margin: "auto",
+            transition: 'opacity 1s ease-out',
+            opacity: show === 'exiting' ? 0 : 1,
+          }}
+          />
+        )
+        }
+      </Transition>
+      <br />
+      {show.modalIsOpen && <Modal show={show.modalIsOpen} closed={() => setShow({ modalIsOpen: false })} />}
+      {show.modalIsOpen && <Backdrop show={show.modalIsOpen} />}
+      <button className="Button" onClick={() => setShow({ modalIsOpen: true })}>Open Modal</button>
+      <h3>Animating Lists</h3>
+      <List />
+    </div>
+  );
 }
 
 export default App;
